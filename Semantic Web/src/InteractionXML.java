359ec5898ab480a8e9418e6938ec635c9be3cea6
Aiebobo <guethomas3@gmail.com>
2015-11-21 12:35:40 +0100

    Add Text to URi

---
+++ Semantic Web/src/InteractionXML.java
@@ -0,0 +1,136 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+public class InteractionXML {
+	
+	Map<Cle, String> entreeURL;
+	
+	public InteractionXML() {
+		entreeURL = new HashMap<Cle, String>();
+	}
+	
+	public void ecritureXML(Document xmlSortie, String nomFichier) throws TransformerConfigurationException{
+		
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		DOMSource source = new DOMSource(xmlSortie);
+		File directory = new File(".");
+	    StreamResult sortie = null;
+		try {
+			System.out.println(directory.getCanonicalPath() + File.separator + nomFichier);
+			sortie = new StreamResult(new File(directory.getCanonicalPath() + File.separator + nomFichier));
+			System.out.println(directory.getCanonicalPath() + File.separator + nomFichier);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    
+	    transformer.setOutputProperty(OutputKeys.VERSION, "1.0");
+	    transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+	    transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");			
+	    		
+	    //formatage
+	    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+			
+	    //sortie
+	    try {
+			transformer.transform(source, sortie);
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+
+	
+	public void sortieFichier(String nomFichier, Map<Cle, List<String>> listeURI) throws TransformerConfigurationException{
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document xmlSortie = builder.newDocument();
+			Element racine = xmlSortie.createElement("listeURI");
+			xmlSortie.appendChild(racine);
+			for (Cle cle : listeURI.keySet()){
+				Element texte = xmlSortie.createElement("texte");
+				texte.setAttribute("id", String.valueOf(cle.getId()));
+				texte.setAttribute("url", String.valueOf(cle.getUrl()));
+				racine.appendChild(texte);
+				for (int i = 0; i < listeURI.get(cle).size(); i++){
+					Element uri = xmlSortie.createElement("URI");
+					uri.setTextContent(listeURI.get(cle).get(i));
+					texte.appendChild(uri);
+				}
+			}
+			
+			ecritureXML(xmlSortie, nomFichier);
+			
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+	public Map<Cle, String> getEntreeURL() {
+		return entreeURL;
+	}
+
+
+
+	public void setEntreeURL(Map<Cle, String> entreeURL) {
+		this.entreeURL = entreeURL;
+	}
+
+
+
+	public Map<Cle,String> entreeFichier(String nomFichier) throws SAXException, IOException{
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			File input = new File(nomFichier);
+			Document document = builder.parse(input);
+			
+			Element racine = document.getDocumentElement();
+			NodeList listeNoeud = racine.getChildNodes();
+
+			for (int i = 0; i < listeNoeud.getLength(); i++){
+				Node texte = listeNoeud.item(i);
+				if(listeNoeud.item(i).getNodeType() == Node.ELEMENT_NODE) {
+			        Element texteElem = (Element) listeNoeud.item(i);
+					Integer id = Integer.parseInt(texteElem.getAttribute("id"));
+					String url = texteElem.getAttribute("url");
+					String text = texte.getTextContent();
+					entreeURL.put(new Cle(id, url), text);
+				}
+			}
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return entreeURL;	
+	}
+}
--- Semantic Web/src/Main.java
+++ Semantic Web/src/Main.java
@@ -1,9 +1,24 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
 
 import org.dbpedia.spotlight.exceptions.AnnotationException;
 import org.dbpedia.spotlight.model.DBpediaResource;
 import org.dbpedia.spotlight.model.Text;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 
 
 public class Main {
@@ -11,20 +26,40 @@ public class Main {
 	
 	public static void main(String [] args)
 	{
-		DBpediaSpotlightClient dBpediaSpotlightClient = new DBpediaSpotlightClient();
-		Text text = new Text("First documented in the 13th century, Berlin was the capital of the Kingdom of Prussia (1701–1918), the German Empire (1871–1918), the Weimar Republic (1919–33) and the Third Reich (1933–45). Berlin in the 1920s was the third largest municipality in the world. After World War II, the city became divided into East Berlin -- the capital of East Germany -- and West Berlin, a West German exclave surrounded by the Berlin Wall from 1961–89. Following German reunification in 1990, the city regained its status as the capital of Germany, hosting 147 foreign embassies.");
+		DBpediaSpotlightClient dBpediaSpotlightClient = new DBpediaSpotlightClient();		
+		InteractionXML interact = new InteractionXML();
+		
+		Map<Cle, String> entree;
+		Map<Cle, List<String>> sortie = new HashMap<Cle, List<String>>();
 		try {
-			List<DBpediaResource> list = dBpediaSpotlightClient.extract(text);
-			System.out.println("Size ::: " + list.size());
-			System.out.println(list.toString());
-			List<String> uriList = new ArrayList<String>();
-			for(DBpediaResource rsrc: list) 
-			{
-				uriList.add(rsrc.getFullUri());
+			entree = interact.entreeFichier("test entree.xml");
+			for (Cle cle : entree.keySet()){
+				String texte = entree.get(cle);
+				Text description = new Text(texte);
+				System.out.println(texte);
+				
+				List<DBpediaResource> list = dBpediaSpotlightClient.extract(description);
+				System.out.println("Size ::: " + list.size());
+				System.out.println(list.toString());
+				List<String> uriList = new ArrayList<String>();
+				for(DBpediaResource rsrc: list) 
+				{
+					uriList.add(rsrc.getFullUri());
+				}
+				System.out.println("Size ::: " + uriList.size());
+				System.out.println(uriList.toString());
+				sortie.put(cle, uriList);
 			}
-			System.out.println("Size ::: " + uriList.size());
-			System.out.println(uriList.toString());
+			interact.sortieFichier("liste_uri.xml", sortie);
+			
+		} catch (SAXException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		} catch (AnnotationException e) {
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
